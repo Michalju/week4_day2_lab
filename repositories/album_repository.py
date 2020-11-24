0b068dfdd5dc634b240935667a1958efea2f73bd
Michalju <juszkin@gmail.com>
2020-11-24 19:30:55 +0000

    Task 4 done

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -1,5 +1,6 @@
 from db.run_sql import run_sql
 from models.album import Album
+import repositories.artist_repository as artist_repository
 
 def save(album):
     sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s,%s) RETURNING *"
@@ -12,3 +13,14 @@ def save(album):
 def delete_all():
     sql = "DELETE  FROM albums" 
     run_sql(sql)
+
+def select(id):
+    album = None
+    sql = "SELECT * FROM albums WHERE id = (%s)"  
+    values = [id] 
+    result = run_sql(sql, values)[0]
+    
+    if result is not None:
+        artist = artist_repository.select(result['artist_id'])
+        album = Album(result['title'], result['genre'], artist, result['id'])
+    return album
