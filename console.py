5d6de5637b4e925a3b21c4faf6ddd48c94c719c3
Michalju <juszkin@gmail.com>
2020-11-24 18:39:03 +0000

    Task 3 done

--- console.py
+++ console.py
@@ -4,18 +4,22 @@ from models.album import Album
 import repositories.artist_repository as artist_repository
 import repositories.album_repository as album_repository
 
-# task_repository.delete_all()
-# user_repository.delete_all()
+# Task 1
+# artist_1 = Artist('Depeche Mode')
+# artist_repository.save(artist_1)
+# artist_2 = Artist('Metallica')
+# artist_repository.save(artist_2)
 
-artist_1 = Artist('Depeche Mode')
-artist_repository.save(artist_1)
-artist_2 = Artist('Metallica')
-artist_repository.save(artist_2)
 
-album_1 = Album('Teens of Denial', 'Rock', artist_1)
-album_repository.save(album_1)
-album_2 = Album('Ride The Lighting', 'Metal', artist_2)
-album_repository.save(album_2)
+# Task 2
+# album_1 = Album('Teens of Denial', 'Rock', artist_1)
+# album_repository.save(album_1)
+# album_2 = Album('Ride The Lighting', 'Metal', artist_2)
+# album_repository.save(album_2)
 
+
+#Task 3
+album_repository.delete_all()
+artist_repository.delete_all()
 
 pdb.set_trace()
